641b5e9a7bebe465c4c866842b6a2febcfdbe5f8
Dr. Chang Liu, PhD <cl91tp@gmail.com>
2022-10-23 12:50:47 +0800

    llvm-toolchain: Invoke llvm assembler and linker instead.
    Instead of invoking clang to do the linking (which is the default
    behavior of CMake), invoke ld.lld directly so that cross-compiling
    is easier. Likewise, instead of invoking the system assembler (as),
    invoke the LLVM builtin assembler.
    
    This is necessary when the system assembler and linker is for a
    different architecture, such as when cross-compiling for x86 on an
    Android aarch64 system.

--- llvm.cmake
+++ llvm.cmake
@@ -20,7 +20,6 @@ set(CMAKE_ASM_COMPILER_ID Clang)
 set(CMAKE_ASM_COMPILER_TARGET ${TRIPLE})
 
 string(APPEND asm_common_flags " -Wno-unused-command-line-argument")
-string(APPEND asm_common_flags " -fno-integrated-as")
 
 set(CMAKE_C_COMPILER "clang")
 set(CMAKE_C_COMPILER_ID Clang)
@@ -33,12 +32,13 @@ set(CMAKE_CXX_COMPILER_TARGET ${TRIPLE})
 string(APPEND c_common_flags " -Wno-sizeof-pointer-div")
 string(APPEND c_common_flags " -Qunused-arguments")
 string(APPEND c_common_flags " -Wno-constant-logical-operand")
-if(NOT ("${TRIPLE}" MATCHES "^riscv"))
-    string(APPEND c_common_flags " -fno-integrated-as")
-endif()
 # clang 11 has a regression in GlobalISel (only used at -O0) affecting the syscall
 # stubs in libsel4runtime; see https://reviews.llvm.org/D83384#2189132
 string(APPEND c_common_flags " -fno-experimental-isel")
+
+set(CMAKE_LINKER "ld.lld")
+set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_C_LINK_EXECUTABLE>")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
